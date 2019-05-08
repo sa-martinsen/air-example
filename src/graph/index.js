ba34e9cb59420a02489d4313ebbcb8934749218f
Полицинский Артем Сергеевич <artifishional@mail.ru>
2019-05-08 17:51:48 +0300

    - graph - added

--- src/graph/index.js
+++ src/graph/index.js
@@ -2,15 +2,15 @@ import { stream } from "m2"
 import { sort } from "nr-graph"
 import Parser from "./parser"
 
-export default ( { schema } ) => stream( (emt) => {
+export default ( { schema } ) => stream( (emt, {sweep}) => {
 
-    schema.get("@$").at( (root) => {
+    sweep.add(schema.get("@$").at( (root) => {
 
         const graph = new Parser().parseData(root);
         const points = sort({ graph });
 
         emt([points]);
 
-    } );
+    } ));
 
 } );
